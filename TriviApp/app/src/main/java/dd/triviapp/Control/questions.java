5f9e2296614324722e5b41dee0ea39b4de3c5932
diegoedelgado <diego.e.delgado@ieee.org>
2016-10-27 20:52:31 -0500

    TriviApp
    Aplicacion Trivia acerca de las maravillas del Mundo

---
+++ TriviApp/app/src/main/java/dd/triviapp/Control/questions.java
@@ -0,0 +1,40 @@
+package dd.triviapp.Control;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import dd.triviapp.R;
+
+public class questions extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public static DatabaseReference getBaseRef() {
+        return FirebaseDatabase.getInstance().getReference();
+    }
+
+}
